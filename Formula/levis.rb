100e1800cd13af2bd97714a3f2ad633ae05ee429
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-10-09 23:28:25 +0700

    Update levis to 1.0.15-alpha

--- Formula/levis.rb
+++ Formula/levis.rb
@@ -1,10 +1,10 @@
 class Levis < Formula
   desc "Kubernetes Manifest Generator"
-  homepage "https://api.github.com/repos/naijab/levis"
-  url "https://github.com/kubeopsskills/levis/releases/download/1.0.25-alpha/levis-1.0.25-alpha-macos-x64.tar.gz"
-  sha256 "steps.shasum-mac-os.outputs.sha"
+  homepage "https://github.com/naijab/levis"
+  url "https://github.com/kubeopsskills/levis/releases/download/1.0.15-alpha/levis-1.0.15-alpha-macos-x64.tar.gz"
+  sha256 "9677bc655ea65b9a3c79c0319e5c94c94cf8c3111828e3f1e90063839bbfbf71"
   license "MIT"
-  version "1.0.25-alpha"
+  version "1.0.15-alpha"
 
   def install
     bin.install "levis"
