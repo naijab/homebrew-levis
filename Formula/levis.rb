8d47593ac60655ae32a6b6204339744009845b0c
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-10-09 21:31:13 +0700

    Update levis to 1.0.22-alpha

--- Formula/levis.rb
+++ Formula/levis.rb
@@ -1,12 +1,14 @@
+
 class Levis < Formula
-  desc "Kubernetes Manifest Generator - CLI"
-  homepage "https://github.com/kubeopsskills/levis"
-  url "https://github.com/naijab/levis.git", tag: "1.0.22-alpha", revision: "7a5dc726c386fed20d8e329382e72dad9ae6cdbf"
-  sha256 "5bd52e9b701d7418dc9d8e8bbefd37a2853c2a6631180bf8324a08ff3de03a29"
+  desc hello world
+  homepage https://github.com/kubeopsskills/levis
+  url https://github.com/naijab/levis/releases/download/1.0.22-alpha/levis-1.0.22-alpha-macos-x64.tar.gz
+  sha256 36217d568c4409504c31c9acad80c193fbab4b5ba22fe0c2ee4ff3523442dbdb
   license "MIT"
-  version "1.0.22-alpha"
+  version 1.0.22-alpha
 
   def install
-    bin.install "levis"
+    bin.install levis
   end
 end
+
